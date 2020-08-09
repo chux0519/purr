7cc82a6d82e02f27c7e169187f61aa5d211f320f
chux0519 <chuxdesign@hotmail.com>
2020-08-09 17:22:33 +0800

    refactor: command line args

--- src/bin/purr.rs
+++ src/bin/purr.rs
@@ -10,7 +10,6 @@ fn main() {
         .arg(
             Arg::with_name("input")
                 .short("i")
-                .long("input")
                 .help("Input Image")
                 .required(true)
                 .takes_value(true),
@@ -18,7 +17,6 @@ fn main() {
         .arg(
             Arg::with_name("output")
                 .short("o")
-                .long("output")
                 .help("Output Image")
                 .required(true)
                 .takes_value(true),
@@ -26,60 +24,71 @@ fn main() {
         .arg(
             Arg::with_name("number")
                 .short("n")
-                .long("number")
                 .help("number of shapes")
                 .required(true)
                 .takes_value(true),
         )
         .arg(
             Arg::with_name("thread")
-                .short("t")
-                .long("thread")
+                .short("j")
                 .help("numebr of threads")
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("shape")
+            Arg::with_name("mode")
+                .short("m")
+                .help("mode: 0=combo, 1=triangle, 2=rect, 3=ellipse, 4=circle, 5=rotatedrect")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("resize")
+                .short("r")
+                .help("input size")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("size")
                 .short("s")
-                .long("shape")
-                .help("shape: triangle/ellipse")
+                .help("output size")
                 .takes_value(true),
         )
         .get_matches();
     let input = matches.value_of("input").unwrap();
     let output = matches.value_of("output").unwrap();
     let shape_number = matches.value_of("number").unwrap().parse().unwrap();
-    let shape = matches.value_of("shape").unwrap_or("triangle");
+    let shape = matches.value_of("mode").unwrap_or("1").parse().unwrap();
     let thread_number = matches
         .value_of("thread")
         .unwrap_or(&num_cpus::get().to_string())
         .parse()
         .unwrap();
+    let input_size = matches.value_of("resize").unwrap_or("256").parse().unwrap();
+    let output_size = matches.value_of("size").unwrap_or("1024").parse().unwrap();
 
-    let model_ctx = PurrContext::new(input, 256, 1024);
+    let model_ctx = PurrContext::new(input, input_size, output_size);
     let mut model_hillclimb = PurrHillClimbModel::new(model_ctx, 1000, 16, 100);
     let mut model_runner: Box<dyn PurrModelRunner<M = PurrHillClimbModel>> = match shape {
-        "triangle" => Box::new(PurrMultiThreadRunner::<Triangle>::new(
+        0 => Box::new(PurrMultiThreadRunner::<Combo>::new(
             shape_number,
             thread_number,
         )),
-        "ellipse" => Box::new(PurrMultiThreadRunner::<Ellipse>::new(
+        1 => Box::new(PurrMultiThreadRunner::<Triangle>::new(
             shape_number,
             thread_number,
         )),
-        "rectangle" => Box::new(PurrMultiThreadRunner::<Rectangle>::new(
+        2 => Box::new(PurrMultiThreadRunner::<Rectangle>::new(
             shape_number,
             thread_number,
         )),
-        "rrectangle" => Box::new(PurrMultiThreadRunner::<RotatedRectangle>::new(
+        3 => Box::new(PurrMultiThreadRunner::<Ellipse>::new(
             shape_number,
             thread_number,
         )),
-        "circle" => Box::new(PurrMultiThreadRunner::<Circle>::new(
+        4 => Box::new(PurrMultiThreadRunner::<Circle>::new(
             shape_number,
             thread_number,
         )),
-        "combo" => Box::new(PurrMultiThreadRunner::<Combo>::new(
+        5 => Box::new(PurrMultiThreadRunner::<RotatedRectangle>::new(
             shape_number,
             thread_number,
         )),
