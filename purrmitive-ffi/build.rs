402297d5ea43b7700b2df249ff4e8d5f517764f6
Yongsheng Xu <chuxdesign@hotmail.com>
2021-11-02 10:32:36 +0800

    fix(ffi): generate C header

--- purrmitive-ffi/build.rs
+++ purrmitive-ffi/build.rs
@@ -5,7 +5,7 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut config: cbindgen::Config = Default::default();
-    config.language = cbindgen::Language::Cxx;
+    config.language = cbindgen::Language::C;
     cbindgen::generate_with_config(&crate_dir, config)
         .unwrap()
         .write_to_file("target/libpurrmitive_ffi.h");
